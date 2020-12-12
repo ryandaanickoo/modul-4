dcf848fe89ecd9967b3467ca22ce7339e6fc505c
tefaryanda <ryanda_nicko_27rpl@student.smktelkom-mlg.sch.id>
2020-12-12 10:38:40 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ul class="nav" style="background-color: #e3f2fd">
+      <li class="nav-item"><router-link class="nav-link" to="/">Home</router-link></li>
+      <li class="nav-item"><router-link class="nav-link" to="/about">About</router-link></li>
+      <li class="nav-item"><router-link class="nav-link" to="/contact">contact</router-link></li>
+    </ul>
+    <router-view></router-view>
+  </div>
+
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1>Contact</h1>
+        <h2>0858 1256 4377</h2>
+        <h2>Ryanda Nicko Al Hikmal/XII RPL2/27</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Contact"
+}
+</script>
